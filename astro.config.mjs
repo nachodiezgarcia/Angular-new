8d88da69c2aa1cf4a8b6858bf30d2cb0b722335d
nachodiezgarcia <diezgarcianacho@gmail.com>
2025-07-11 07:54:23 +0200

    Cambios json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import tailwindcss from '@tailwindcss/vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+  }
+});
