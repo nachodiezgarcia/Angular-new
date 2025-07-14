c2e232b89c8ab638bd3ef077e95a61bc600b7bf5
nachodiezgarcia <diezgarcianacho@gmail.com>
2025-07-14 11:29:02 +0200

    App cursos

--- src/api/trainings.api.ts
+++ src/api/trainings.api.ts
@@ -1,17 +1,23 @@
 import client from "@/lib/client";
-import type { Lesson, Training } from "./model";
-
-
-
-export const getTrainings = async (): Promise<Training[]> =>
-    await client.getContentList<Training>({ contentType:"training" });
-
-
-
-
+import type { Lesson, Training, TrainingWithLessons } from "./model";
 
 export const getLessons = async (ids: string[]): Promise<Lesson[]> =>
     client.getContentList<Lesson>({
         contentType: "lesson",
         id: { in: ids }
 });
+
+export const getTrainings = async (): Promise<TrainingWithLessons[]> => {
+    const trainings = await client.getContentList<Training>({ contentType: "training" });
+    const trainingWithLessons = await Promise.all(
+        trainings.map(async (training) => {
+                const lessons = await getLessons(training.lessons);
+                return {
+                    ...training,
+                    lessons
+                }
+            }        
+        )
+    );
+    return trainingWithLessons;
+}
